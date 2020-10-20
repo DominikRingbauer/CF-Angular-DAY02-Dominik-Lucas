108c55d3bce21b1df1e0d4240471f7eeb4e79641
Dominik Ringbauer <dominik.ringbauer@gmx.at>
2020-10-20 14:46:15 +0200

    finished

--- CarRental/src/app/contact/contact.component.ts
+++ CarRental/src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -8,22 +8,27 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class ContactComponent implements OnInit {
 	info = new FormGroup({
-	   firstName: new FormControl(''),
-	   lastName: new FormControl(''),
-	   age: new FormControl(''),
-	   email:new FormControl(''),
-	   text:new FormControl(''),
+	   firstName: new FormControl('',Validators.required),
+	   lastName: new FormControl('',Validators.required),
+	   age: new FormControl('',Validators.required),
+	   email:new FormControl('',Validators.required),
+	   text:new FormControl('',Validators.required),
 	});
 
   constructor() { }
 
+  onSubmit(){
+
+    if(this.info.valid){
+      var a = this.info.value;
+      console.log(a)
+   }
+
+  }
+
   ngOnInit(): void {
   }
 
-  onSubmit(){
-
-      var a = this.info.value;
-      console.log(a);
-  }
+  
 
 }
--- CarRental/src/app/home/home.component.ts
+++ CarRental/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+	info = new FormGroup({
+	   image: new FormControl('',Validators.required),
+	   brand: new FormControl('',Validators.required),
+	   type: new FormControl('',Validators.required),
+	   color:new FormControl('',Validators.required),
+	   price:new FormControl('',Validators.required),
+	});
+
 	cars = [{
 		brand:"Audi",
 		type:"A8",
@@ -34,7 +43,15 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
-  
+  addCar(){
+
+    if(this.info.valid){
+      var a = this.info.value;
+      this.cars.push(a);
+      console.log(this.cars);
+   }
+
+  }
 
   ngOnInit(): void {
   }
